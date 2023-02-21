444588347db1c357631a7d32fdace0bead0916d7
David Marsh <40401344+Daveym1@users.noreply.github.com>
2023-02-21 20:34:32 +0000

    made type buttons hidden before category clicked

--- src/components/SearchCategories/SearchCategories.js
+++ src/components/SearchCategories/SearchCategories.js
@@ -77,13 +77,17 @@ const categories = [
   },
 ];
 
-// useState hooks for checking the selected category
+// useState hooks for checking the selected category and whether a category button has been clicked
 
 const SearchCategories = ({ handleTypeSelect }) => {
   const [selectedCategory, setSelectedCategory] = useState(categories[0]);
+  const [categoryClicked, setCategoryClicked] = useState(false);
+  const [resultsFound, setResultsFound] = useState(true);
 
   const handleCategorySelect = (category) => {
     setSelectedCategory(category);
+    setCategoryClicked(true);
+    setResultsFound(true);
   };
 
   const handleTypeSelectLocal = (type) => {
@@ -96,6 +100,18 @@ const SearchCategories = ({ handleTypeSelect }) => {
     return newName;
   }
 
+  const filteredTypes = categories.filter(
+    (category) => category.name === selectedCategory.name
+  )[0].types;
+
+  const handleResultsFound = (results) => {
+    if (results.length === 0) {
+      setResultsFound(false);
+    } else {
+      setResultsFound(true);
+    }
+  };
+
   return (
     <div className="categories-container">
       <div className="categories">
@@ -106,24 +122,27 @@ const SearchCategories = ({ handleTypeSelect }) => {
             className={
               selectedCategory.name === category.name
                 ? "category active"
-                : "category"
+                : "category p-3 "
             }
           >
             {category.name}
           </button>
         ))}
       </div>
-      <div className="types">
-        {selectedCategory.types.map((type) => (
-          <button
-            key={type}
-            onClick={() => handleTypeSelectLocal(type)}
-            className="type bg-red-300 border-2"
-          >
-            {getName(type)}
-          </button>
-        ))}
-      </div>
+      {categoryClicked && (
+        <div className="types">
+          {filteredTypes.map((type) => (
+            <button
+              key={type}
+              onClick={() => handleTypeSelectLocal(type)}
+              className="type inline-block rounded bg-primary px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-black shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
+            >
+              {getName(type)}
+            </button>
+          ))}
+          {!resultsFound && <div className="no-results">No results found.</div>}
+        </div>
+      )}
     </div>
   );
 };
